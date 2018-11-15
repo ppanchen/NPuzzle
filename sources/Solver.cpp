441df192694866a258a7ee29db7402735b305b29
timchuchok <timchenko.vlad@gmail.com>
2018-11-15 23:17:06 +0200

    Good job (#4)

--- sources/Solver.cpp
+++ sources/Solver.cpp
@@ -16,14 +16,26 @@ Solver::Solver(std::shared_ptr<Board> initial)
     if(!initial->isSolvable())
         return;
 
-    auto tmp = [](std::shared_ptr<Item> i1, std::shared_ptr<Item> i2)
+    auto tmp = [&](std::shared_ptr<Item> i1, std::shared_ptr<Item> i2)
             {
+//                if (i1.get()->_prevBoard == nullptr)
+//                    return true;
+//                if (i2.get()->_prevBoard == nullptr)
+//                    return false;
                 int steps1 = listDepth(*(i1.get()));
                 int steps2 = listDepth(*(i2.get()));
-                if (steps1 != steps2)
-                    return steps1 < steps2;
+
+                int a1 = steps1 + i1->_board->h();
+                int a2 = steps2 + i2->_board->h();
+
+                if (a1 == a2)
+                    return i1->_board->h() < i2->_board->h();
                 else
-                    return i1->_board->h() > i2->_board->h();
+                    return a1 > a2;
+//                if (steps1 != steps2)
+//                    return steps1 < steps2;
+//                else
+                    return steps1 + i1->_board->h() > steps2 + i2->_board->h();
             };
 
     std::priority_queue<std::shared_ptr<Item>, std::deque<std::shared_ptr<Item>>, decltype(tmp)> priorityQueue(tmp);
@@ -32,22 +44,22 @@ Solver::Solver(std::shared_ptr<Board> initial)
     while (true)
     {
         std::shared_ptr<Item> board = priorityQueue.top();
-        std::cout << *(board->_board) << std::endl;
-        std::getchar();
+//        std::cout << *(board->_board) << std::endl;
+//        std::getchar();
         if(board->_board->isGoal()) {
             itemToList(Item(board, board->_board));
             return;
         }
-
+        priorityQueue.pop();
         auto neighbors = board->_board->neighbors();
+        int i = 0;
         for (auto const &neighbor : neighbors)
         {
-
-            //оптимизация. Очевидно, что один из соседей - это позиция
-            // которая была ходом раньше. Чтобы не возвращаться в состояния,
-            // которые уже были делаем проверку. Экономим время и память.
-            if(neighbor != nullptr && !containsInPath(*board, *neighbor))
-                priorityQueue.push(std::make_shared<Item>(board, neighbor));
+            bool contain = containsInPath(*board, neighbor.get());
+            if (contain)
+                i++;
+            if(neighbor != nullptr && !contain)
+                priorityQueue.push(std::make_shared<Item>(Item(board, neighbor)));
         }
     }
 }
@@ -100,12 +112,14 @@ void Solver::itemToList(Solver::Item item)
     }
 }
 
-bool Solver::containsInPath(Solver::Item item, Board board)
+bool Solver::containsInPath(Solver::Item item, Board *board)
 {
+    if (board == nullptr)
+        return true;
     Item *item2 = &item;
     while (true)
     {
-        if (*item2->_board == board)
+        if (*item2->_board == *board)
             return true;
         item2 = item2->_prevBoard.get();
         Logger::timeCount++;
