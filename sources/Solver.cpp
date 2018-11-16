47fc1dc4f1d469649c9e3c1623b961f814ab0a2b
ppanchen <mrpeterpan10@gmail.com>
2018-11-16 23:43:58 +0200

    - examples added - some fixes

--- sources/Solver.cpp
+++ sources/Solver.cpp
@@ -16,12 +16,8 @@ Solver::Solver(std::shared_ptr<Board> initial)
     if(!initial->isSolvable())
         return;
 
-    auto tmp = [&](std::shared_ptr<Item> i1, std::shared_ptr<Item> i2)
+    auto tmp = [](std::shared_ptr<Item> i1, std::shared_ptr<Item> i2)
             {
-//                if (i1.get()->_prevBoard == nullptr)
-//                    return true;
-//                if (i2.get()->_prevBoard == nullptr)
-//                    return false;
                 int steps1 = listDepth(*(i1.get()));
                 int steps2 = listDepth(*(i2.get()));
 
@@ -32,10 +28,6 @@ Solver::Solver(std::shared_ptr<Board> initial)
                     return i1->_board->h() < i2->_board->h();
                 else
                     return a1 > a2;
-//                if (steps1 != steps2)
-//                    return steps1 < steps2;
-//                else
-                    return steps1 + i1->_board->h() > steps2 + i2->_board->h();
             };
 
     std::priority_queue<std::shared_ptr<Item>, std::deque<std::shared_ptr<Item>>, decltype(tmp)> priorityQueue(tmp);
@@ -44,23 +36,29 @@ Solver::Solver(std::shared_ptr<Board> initial)
     while (true)
     {
         std::shared_ptr<Item> board = priorityQueue.top();
-//        std::cout << *(board->_board) << std::endl;
-//        std::getchar();
         if(board->_board->isGoal()) {
             itemToList(Item(board, board->_board));
             return;
         }
+//        std::cout << *(board->_board) << std::endl;
         priorityQueue.pop();
         auto neighbors = board->_board->neighbors();
-        int i = 0;
         for (auto const &neighbor : neighbors)
         {
             bool contain = containsInPath(*board, neighbor.get());
-            if (contain)
-                i++;
             if(neighbor != nullptr && !contain)
-                priorityQueue.push(std::make_shared<Item>(Item(board, neighbor)));
+                priorityQueue.push(std::make_shared<Item>(board, neighbor));
         }
+        Logger::timeCount++;
+        int size = 0;
+        auto temp = priorityQueue;
+        while (!temp.empty())
+        {
+            size += listDepth(*temp.top().get());
+            temp.pop();
+        }
+        if (size > Logger::memoryCount)
+            Logger::memoryCount = size;
     }
 }
 
@@ -89,7 +87,6 @@ int Solver::listDepth(Solver::Item item)
     {
         c++;
         item2 = item2->_prevBoard.get();
-        Logger::timeCount++;
         if(item2 == nullptr)
         {
             return c;
@@ -103,7 +100,6 @@ void Solver::itemToList(Solver::Item item)
     while (true)
     {
         item2 = item2->_prevBoard.get();
-        Logger::timeCount++;
         if (item2 == nullptr)
         {
             return;
@@ -122,7 +118,6 @@ bool Solver::containsInPath(Solver::Item item, Board *board)
         if (*item2->_board == *board)
             return true;
         item2 = item2->_prevBoard.get();
-        Logger::timeCount++;
         if (item2 == nullptr)
             return false;
     }
